e22f4f586eb8e3f6be7817e95767052f36b74d53
kristinaordenans <okristin92@gmail.com>
2023-06-26 19:26:33 +0300

    Home

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,40 @@
+import { Routes, Route, Link } from "react-router-dom";
+import { Home } from "pages/Home";
+import { Movies } from "pages/Movies";
+import { Movie } from "pages/Movie";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/movies">Movies</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home/>}/> 
+        <Route path="/movies" element={<Movies/>}>
+          <Route path="/movies/:movieId" element={<Movie/>}/>
+          <Route path="" element={<div>Cast</div>}/>
+          <Route path="" element={<div>Reviews</div>}/>
+        </Route>   
+      </Routes>
     </div>
   );
 };
+
+
+// const fetch = require('node-fetch');
+
+// const url = 'https://api.themoviedb.org/3/trending/all/day?language=en-US';
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMDg3Zjc1N2VkNzlmMjJjYzc1MmNjNjI2MDRiYmEzMSIsInN1YiI6IjY0OTk2MDQyYmJkMGIwMDBhZGYxNGNiNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._8EWd1WEkvFSv-1esa_lWyYW1ErfKyLO7K4zWbUx18Y'
+//   }
+// };
+
+// fetch(url, options)
+//   .then(res => res.json())
+//   .then(json => console.log(json))
+//   .catch(err => console.error('error:' + err));
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+export const Home = () => {
+
+//  список популярних фільмів
+    return <div>
+        <h2>Trending today</h2>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+}
